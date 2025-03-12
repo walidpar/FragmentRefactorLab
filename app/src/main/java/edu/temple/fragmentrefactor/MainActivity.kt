1afa3d1f847a6258548b7112ba86c8f1f2b00e07
Walid Parast <walidparast1@gmail.com>
2025-03-12 15:52:20 -0400

    Added fragmentTransaction, completed assignment.

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -2,12 +2,20 @@ package edu.temple.fragmentrefactor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // No need for FragmentTransaction if the fragment is declared in XML
+        if (supportFragmentManager.findFragmentById(R.id.fragmentContainer) == null) {
+
+            val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(R.id.fragmentContainer, RefactorFragment())
+
+            fragmentTransaction.commit()
+        }
     }
 }
